5cb29f7c02476af1df272ec7146169483c70c690
indreshharshan26-ai <indreshharshan26@gmail.com>
2026-09-25 10:57:01 +0530

    Day 4 of Mastering Java

--- OOPS/Abstraction/abb.java
+++ OOPS/Abstraction/abb.java
@@ -1,7 +1,7 @@
 abstract class Sample {
 
     abstract void sound();  //abstract method has no body
-    abstract void set();
+    abstract void set();  
 }
 
 class Sample2 extends Sample {
@@ -20,6 +20,7 @@ public class abb {
     public static void main(String[] args) {
 
         Sample obj = new Sample2();
+        // sample kulla erukura abstract class func mattum than access pana mudiyum 
         
         obj.sound();
         obj.set();
